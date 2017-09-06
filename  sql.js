420d760268ee3d76cdb253552f5e3afedd33c6c6
clouds8 <clouds8@sina.cn>
2017-09-06 23:38:08 +0800

    add base method.

---  sql.js
+++  sql.js
@@ -7,6 +7,28 @@ class SQL {
 		this.TABLE = table;
 	}
 
+	get(params, operation, obj) {
+		let query = db(this.TABLE).select().limit(20);
+		switch (arguments.length) {
+			case 1: {
+				query.where(params);
+				break;
+			}
+			case 2: {
+				query.where(params, operation);
+				break;
+			}
+			case 3: {
+				query.where(params, operation, obj);
+				break;
+			}
+			default: {
+				break;
+			}
+		}
+		return query;
+	}
+
 	find(id) {
 		let query = db.select().from(this.TABLE)
 		if (id) {
@@ -19,10 +41,21 @@ class SQL {
 		return db(this.TABLE).insert(params);
 	}
 
+	update(updateValue, params) {
+		return db(this.TABLE).update(updateValue).where(params);
+	}
+
+	delete(id) {
+		return db(this.TABLE).delete().where('id', id);
+	}
+
 	getDB() {
 		return db(this.TABLE);
 	}
 
+	getConn() {
+		return db;
+	}
 
 }
 
